f6f1b028d3a2555de35c124040d9e364c7c95014
Joao Marono <the_real_kazan@protonmail.com>
2020-11-13 12:32:14 +0000

    birthday reminderdone

--- 1-birthday-reminder/setup/src/App.js
+++ 1-birthday-reminder/setup/src/App.js
@@ -2,16 +2,26 @@ import React, { useState } from 'react'
 import data from './data'
 import List from './List'
 import './index.css'
+
 function App() {
   const [people, setPeople] = useState(data)
+
+  const handlerChange = () => {
+    document.getElementById('birthdayList').innerHTML =
+      'There are no more birthdays!'
+
+    document.getElementById('numberBirthdays').innerHTML = 0
+  }
 
   return (
     <main>
       <div className='container'>
-        <h3>{people.length} birthdays today</h3>
+        <h3>
+          <div id='numberBirthdays'>{people.length}</div> birthdays today
+        </h3>
         <List people={people} />
-        {people.length == 0 ? <h5>There are no birthdays left today</h5> : null}
-        <button type='button' onClick={() => setPeople([])}>
+        <div id='noMoreBirthdays'></div>
+        <button type='button' onClick={() => handlerChange()}>
           Clear All
         </button>
       </div>
--- 1-birthday-reminder/setup/src/List.js
+++ 1-birthday-reminder/setup/src/List.js
@@ -1,22 +1,43 @@
 import './index.css'
-import React from 'react'
-import data from './data'
+import React, { useState } from 'react'
 
-const List = (props) => {
+const List = ({ people }) => {
+  const [values, setValues] = useState(people)
+
+  const handleClick = (id) => {
+    let newPeople = values.filter((e) => {
+      return e.id !== id
+    })
+
+    setValues(newPeople)
+
+    document.getElementById('numberBirthdays').innerHTML = newPeople.length
+
+    if (newPeople.length === 0) {
+      document.getElementById('noMoreBirthdays').innerHTML =
+        'There are no more birthdays!'
+    }
+  }
+
   return (
-    <>
-      {props.people.map((e) => {
+    <div id='birthdayList'>
+      {values.map((e) => {
+        const { image, id, name, age } = e
+
         return (
-          <div className='person'>
-            <img src={e.image} alt='image' />
+          <div className='person' key={id}>
+            <img src={image} alt='' />
             <section>
-              <h4>{e.name}</h4>
-              <p>{e.age} years</p>
+              <h4>{name}</h4>
+              <p>{age} years</p>
+              <button type='button' onClick={() => handleClick(id)}>
+                Delete Birthday
+              </button>
             </section>
           </div>
         )
       })}
-    </>
+    </div>
   )
 }
 
